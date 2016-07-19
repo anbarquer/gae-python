6778ba4ba4cd5d3424874540eb1069d0eb03e711
Antonio Barquero <antonio.barquero@intelygenz.com>
2016-07-19 12:50:25 +0200

    Tests + minor changes

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import os
 import jinja2
-import urllib
 import random
 
-from config import VIEWS, URLS
+from config import VIEWS, URLS, NAME_VALUES, PRICE_VALUES
 from models import AppUser, Product, ndb
 
 from google.appengine.api import users
@@ -61,38 +44,38 @@ class ProductsHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         products = Product.query().order(Product.cost)
-        app_user = None
         if user:
             app_user = AppUser.get_or_insert(get_user_key(user),
                                              identity=user.user_id(),
                                              email=user.email())
+        else:
+            return self.redirect(URLS['index'])
         template = JINJA_ENVIRONMENT.get_template(VIEWS['products'])
         self.response.write(template.render({'products': products, 'user': app_user}))
 
     def post(self):
         user = users.get_current_user()
         action = self.request.POST.get('action')
-        if user:
-            if action == 'create':
-                product = Product(name='product ' + str(random.randint(1, 20)), cost=random.randint(1, 100))
-                product.put()
+        if action == 'create':
+            product = Product(name='p' + str(random.randint(NAME_VALUES[0], NAME_VALUES[1])),
+                              cost=random.randint(PRICE_VALUES[0], PRICE_VALUES[1]))
+            product.put()
+        elif user:
+            product = Product.get_by_id(int(self.request.POST.get('id')))
+            app_user = AppUser.get_or_insert(get_user_key(user),
+                                             identity=user.user_id(),
+                                             email=user.email())
+            if action == 'buy':
+                app_user.add_product(product)
 
-            else:
-                product = Product.get_by_id(int(self.request.POST.get('id')))
-                app_user = AppUser.get_or_insert(get_user_key(user),
-                                                 identity=user.user_id(),
-                                                 email=user.email())
-                if action == 'buy':
-                    app_user.add_product(product)
-
-                elif action == 'delete':
-                    app_user.remove_product(product)
-                    ndb.Key(Product, product.key.id()).delete()
+            elif action == 'delete':
+                app_user.remove_product(product)
+                ndb.Key(Product, product.key.id()).delete()
         self.redirect(URLS['products'])
 
 
 app = webapp2.WSGIApplication([
-    (URLS['root'], MainHandler),
+    (URLS['index'], MainHandler),
     (URLS['products'], ProductsHandler),
 
 ], debug=True)
--- config.py
+++ config.py
@@ -3,19 +3,16 @@ BASE_TEMPLATES = 'templates'
 
 # TEMPLATES
 INDEX = BASE_TEMPLATES + '/index.html'
-LOGIN = BASE_TEMPLATES + '/login.html'
-REGISTER = BASE_TEMPLATES + '/register.html'
-DASHBOARD = BASE_TEMPLATES + '/dashboard.html'
 PRODUCTS = BASE_TEMPLATES + '/products.html'
 
 # URLS
-ROOT_URL = '/'
-INDEX_URL = '/index'
-LOGIN_URL = '/login'
-REGISTER_URL = '/register'
-DASHBOARD_URL = '/dashboard'
+INDEX_URL = '/'
 PRODUCTS_URL = '/products'
 
 # VIEWS
-VIEWS = {'index': INDEX, 'login': LOGIN, 'register': REGISTER, 'dashboard': DASHBOARD, 'products': PRODUCTS}
-URLS = {'root':ROOT_URL, 'index': INDEX_URL, 'login': LOGIN_URL, 'register': REGISTER_URL, 'dashboard': DASHBOARD_URL, 'products': PRODUCTS_URL}
+VIEWS = {'index': INDEX, 'products': PRODUCTS}
+URLS = {'index': INDEX_URL, 'products': PRODUCTS_URL}
+
+# PRODUCTS STATS
+NAME_VALUES = [1, 20]
+PRICE_VALUES = [1, 100]
